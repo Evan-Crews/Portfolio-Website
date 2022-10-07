703aa12056e9d3e3e26615b5e953258e042764bd
EvanCrews <evancrewscoding@gmail.com>
2022-10-07 16:54:47 -0700

    added the resume and button for downloading it

--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -5,6 +5,7 @@ function MenuBar() {
   return (
     <div className="mainMenu">
       <button className="resumeButton">Download Resume</button>
+      <a className="resumeButton" href={require("../../Files/Evan Crews-Resume.pdf")} download="myResume">Download file</a>
       <a href="https://www.linkedin.com/in/evan-crews/">
         <img src={linkedIn} id="linkedIn" alt="LinkedIn" size="sm" className="buttonIcons"/>
       </a>
