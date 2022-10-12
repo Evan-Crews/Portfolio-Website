8d670b87edcc8d845baca6c2572e4d9ce3d04919
EvanCrews <evancrewscoding@gmail.com>
2022-10-12 11:27:25 -0700

    created bio portion

--- src/Components/Biography/Biography.js
+++ src/Components/Biography/Biography.js
@@ -3,6 +3,11 @@ import './Biography.scss'
 function Biography () {
   return (
     <div className='biographyContainer'>
+      <div className='introDiv'>
+        <h1 className='introText'>
+          Hello, I'm Evan, Front-End Developer
+        </h1>
+      </div>
       <p className='bioInfo'>
         This is a test paragraph for the purposes of testing the visibility of the component within the application for this portfolio website.
       </p>
